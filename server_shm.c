e8f1550de206fe02bf950090a793f438fb0837c3
xuyg74 <80707525@qq.com>
2018-08-22 11:17:26 +0800

    Open file in the server_shm

--- server_shm.c
+++ server_shm.c
@@ -1,29 +1,36 @@
-#include "comm.h"
-#include <unistd.h>
+#include"comm.h"
+#include<unistd.h>
+#include<string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#include <sys/wait.h>
-#include "sem_comm.h"
+#include<sys/wait.h>
+#include"sem_comm.h"
+#include <errno.h>
 
 int main()
 {
 	int shmid = creatShm();
 	char* mem = (char*)shmat(shmid, NULL, 0);
+	if(NULL == mem){
+		printf("Failed to create SHM!\n");
+	} 
 
 	int semid = creatSemSet(1);
 	initSem(semid,0);
-#if 1
+	sleep(10);
+#if 0
 	int i = 1;
 	while(1)
 	{
-		sleep(2);
+		usleep(1000);
 		P(semid,0);
 		if(mem[0] == 0x00){
 			mem[0] = 0x1;
 			mem[i++] = 'A';
 			i %= (SIZE-1);
 			mem[i] = '\0';
-		} else {
-			printf("The data is not updated!\n");
 		}
 		V(semid,0);
 	}
@@ -38,6 +45,42 @@ int main()
 		mem[3] = 'D';
 		mem[4] = '\0';
 	}
+#endif
+
+#if 1
+//	int length = (int)strlen(mem);
+	char *filename = "/media/sf_share/exam/example";
+	int fp = open(filename,O_RDWR,S_IWUSR);
+	if(-1 == fp){
+		printf("The file %s can't be open.\n", filename);
+	} else {
+		printf("The file %s has been open.\n", filename);
+	}
+
+	while(1){
+		usleep(1000);
+		P(semid,0);
+		if(mem[0] == 0x00){
+			int num_bytes = read(fp, mem+1, SIZE-2);
+			if(num_bytes==0){
+				printf("The file %s has been finished.\n", filename);
+				close(fp);
+				V(semid,0);
+				break;
+			}else if(num_bytes == -1){
+				printf("The file %s has error to be read. errno: %d!\n", filename, errno);
+				close(fp);
+				V(semid,0);
+				break;
+			} else {
+				printf("%d bytes has been read!\n", num_bytes);
+				fflush(stdout);
+			}
+			mem[0] = 0x1;
+		}
+		V(semid,0);
+	}
+
 #endif
 	shmdt(mem);
 	destoryShm(shmid);
